7070ff2441394b5bcb5c7497c96d649ca06e6906
babarbaigmz <babarbaigmz@gmail.com>
2023-03-26 15:33:06 +1100

    init

--- xetra/common/file_operations.py
+++ xetra/common/file_operations.py
@@ -7,16 +7,27 @@ Instance Variables:
 
 Instance Methods:
 """
-
+import pandas
 from pathlib import Path
+from typing import List
 
 
 class FileOperations:
     """
     To interact with files on local folders
     """
-    def __init__(self,):
+
+    def __init__(self, file_path: str):
+        """
+        :param file_path: Local file path
+        """
+        self.file_path = file_path
+
+    def list_files_in_location(self):
         pass
 
+    def read_csv_to_df(self):
+        pass
 
-
+    def write_df_to_location(self):
+        pass
---
+++ xetra/common/meta_process.py
@@ -0,0 +1,17 @@
+"""
+Methods to process meta file for process control meta_file.csv
+"""
+
+
+class MetaProcess:
+    """
+    Class to process meta file for process control meta_file.csv
+    """
+    @staticmethod
+    def update_meta_file():
+        pass
+
+    @staticmethod
+    def return_date_list():
+        pass
+

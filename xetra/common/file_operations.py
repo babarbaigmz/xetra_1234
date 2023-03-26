3d6df335a18cf206c972816bdb88415235c77b5a
babarbaigmz <babarbaigmz@gmail.com>
2023-03-26 15:14:41 +1100

    Init

---
+++ xetra/common/file_operations.py
@@ -0,0 +1,22 @@
+"""
+Class to store method related to files read and write operations
+S3 bucket methods can also be implemented
+FileOperations class is similar to S3BucketConnector class in course
+
+Instance Variables:
+
+Instance Methods:
+"""
+
+from pathlib import Path
+
+
+class FileOperations:
+    """
+    To interact with files on local folders
+    """
+    def __init__(self,):
+        pass
+
+
+

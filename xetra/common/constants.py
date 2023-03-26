d25a7d26f1651934fbb81b4f3cacbd3acfb015fd
babarbaigmz <babarbaigmz@gmail.com>
2023-03-26 16:05:02 +1100

    init

---
+++ xetra/common/constants.py
@@ -0,0 +1,23 @@
+"""
+File to Store constants
+"""
+from enum import Enum
+
+
+class FileTypes(Enum):
+    """
+    supported file types for process
+    """
+    CSV = 'csv'
+    PARQUET = 'parquet'
+
+
+class MetaProcessFormat(Enum):
+    """
+    formation for MetaProcess class
+    """
+    META_DATE_FORMAT = '%Y-%m-%d'
+    META_PROCESS_FORMAT = '%Y-%m-%d %H:%M:%S'
+    META_SOURCE_DATE_COL = 'source_date'
+    META_PROCESS_COL = 'datetime_of_processing'
+    META_FILE_FORMAT = 'csv'

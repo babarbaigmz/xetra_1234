890011d1753ac152d52108ab68c82e30e940165b
babarbaigmz <babarbaigmz@gmail.com>
2023-04-10 13:54:08 +1000

    init

--- configs/config.py
+++ configs/config.py
@@ -4,10 +4,6 @@ Process to read yaml configuration file
 
 import yaml
 from pathlib import Path
-import logging
-import logging.config
-
-from datetime import datetime
 
 YAML_FILE_PATH = r'xetra_config.yaml'
 
@@ -24,20 +20,3 @@ class Config:
                 self.yaml_file = yaml.safe_load(yaml_handler)
         except FileNotFoundError:
             raise
-
-    def get_value(self, p_field_name):
-        return self.yaml_file.get(p_field_name)
-
-
-def main():
-    config = Config()
-    log_file_name = f"./logs/{(config.yaml_file['handlers']['filehandler']['filename']).split('.')[0]}_{datetime.strftime(datetime.now().astimezone(), '%Y-%m-%d %H-%M-%S')}.{(config.yaml_file['handlers']['filehandler']['filename']).split('.')[-1]} "
-    config.yaml_file['handlers']['filehandler']['filename'] = log_file_name
-    logging.config.dictConfig(config.yaml_file)
-    logger = logging.getLogger(__name__)
-    logger.info("This is a test")
-
-
-if __name__ == '__main__':
-    main()
-
--- configs/process_logger.py
+++ configs/process_logger.py
@@ -1,34 +1,27 @@
 """
-Logging Module
+Configure Logging for Xetra Project
 """
 import logging
 import logging.config
 from pathlib import Path
 from configs.config import Config
+from datetime import datetime
 
 
 class ProcessLog:
 
-    def __init__(self, file_path: Path):
-        self.logger = logging.getLogger(__name__)
-        self.file_path = file_path
-        self.logger.setLevel(logging.INFO)
-        # Create handlers
-        self.console_handler = logging.StreamHandler()
-        self.file_handler = logging.FileHandler(filename=self.file_path, mode='w')
-
-        # Create formatters
-        self.formatter = logging.Formatter(
-            fmt='%(asctime)s - %(levelname)s - %(message)s', datefmt='%d-%b-%Y %H:%M:%S')
-        self.console_handler.setFormatter(self.formatter)
-        self.file_handler.setFormatter(self.formatter)
-
-        # Add handlers to the logger
-        self.logger.addHandler(self.console_handler)
-        self.logger.addHandler(self.file_handler)
+    def __init__(self, file_path: str):
+        self.__file_path = file_path
+        self.__logging_config = Config()
+        self.__log_file_name = Path(self.__file_path,
+                                    f"{(self.__logging_config.yaml_file['handlers']['filehandler']['filename']).split('.')[0]}_{datetime.strftime(datetime.now().astimezone(), '%Y-%m-%d %H-%M-%S')}.{(self.__logging_config.yaml_file['handlers']['filehandler']['filename']).split('.')[-1]}")
+        self.__logging_config.yaml_file['handlers']['filehandler']['filename'] = self.__log_file_name
+        print(self.__logging_config.yaml_file)
+        logging.config.dictConfig(self.__logging_config.yaml_file)
+        self.__logger = logging.getLogger(__name__)
 
     def log_message(self, message: str = None, exec_info: bool = False):
         if exec_info:
-            self.logger.error(msg=message, exc_info=True)
+            self.__logger.error(msg=message, exc_info=True)
         else:
-            self.logger.info(msg=message)
+            self.__logger.info(msg=message)
